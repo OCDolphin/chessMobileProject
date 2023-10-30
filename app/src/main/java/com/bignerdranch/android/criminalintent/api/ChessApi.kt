0141479b6d8b057a3b8e94e01e0e0853c02551fd
Phillip Davis <phdavis1027@gmail.com>
2023-10-30 13:57:07 -0400

    fix: no  at start of GET annotations

--- app/src/main/java/com/bignerdranch/android/criminalintent/api/ChessApi.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/api/ChessApi.kt
@@ -6,9 +6,9 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ChessApi {
-  @GET("/titled/{rankAbbrev}")
+  @GET("titled/{rankAbbrev}")
   suspend fun getTitledPlayers(@Path(value="rankAbbrev") rankAbbrev: String): Response<ChessPlayersResult>
 
-  @GET("/player/{username}/stats")
+  @GET("player/{username}/stats")
   suspend fun getPlayerByName(@Path(value="username") username: String): Response<ChessPlayerResult>
 }
